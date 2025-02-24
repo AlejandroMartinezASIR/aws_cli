253fef1b0a36f00dba542ce1404d1f3e5a4cf4d4
AlejandroMartinezASIR <alejandromartinezasir@gmail.com>
2025-02-24 13:00:56 +0100

    ip

--- 05-create_elastic_ip.sh
+++ 05-create_elastic_ip.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -x
 
+# Deshabilitamos la paginación de la salida de los comandos de AWS CLI
 export AWS_PAGER=""
 
+#Leemos las variables de entorno
 source .env
 
+#Obtenemos la ID de la instancia a partir de su nombre
 INSTANCE_ID=$(aws ec2 describe-instances \
-            --filters "Name=tag:Name,Values=$INSTANCE_NAME_FRONTEND" \
+            --filters "Name=tag:Name,Values=$INSTANCE_NAME_BALANCEADOR" \
                       "Name=instance-state-name,Values=running" \
             --query "Reservations[*].Instances[*].InstanceId" \
             --output text)
 
+#Creamos la IP elastica
 ELASTIC_IP=$(aws ec2 allocate-address --query PublicIp --output text)
 
+#Asociamos la IP para la instancia del balanceador de carga
 aws ec2 associate-address --instance-id $INSTANCE_ID --public-ip $ELASTIC_IP
